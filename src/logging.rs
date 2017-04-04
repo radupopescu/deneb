8f5b3abf600be4455cd2f5b3f1e33fbf63d7e757
Radu Popescu <mail@radupopescu.net>
2017-04-04 23:57:13 +0200

    Enable JSON encoder for logging

--- src/logging.rs
+++ src/logging.rs
@@ -1,15 +1,15 @@
 use log::LogLevelFilter;
 use log4rs::append::console::ConsoleAppender;
+use log4rs::encode::json::JsonEncoder;
 use log4rs::config::{Appender, Config, Root};
 
 use errors::*;
 
 pub fn init() -> Result<()> {
-    let stdout = ConsoleAppender::builder().build();
+    let json_encoder = Box::new(JsonEncoder::new());
+    let stdout = ConsoleAppender::builder().encoder(json_encoder).build();
 
-    let config = Config::builder()
-        .appender(Appender::builder()
-                  .build("stdout", Box::new(stdout)))
+    let config = Config::builder().appender(Appender::builder().build("stdout", Box::new(stdout)))
         .build(Root::builder()
                .appender("stdout")
                // Just enable all logging levels for now
@@ -18,4 +18,3 @@ pub fn init() -> Result<()> {
     let _ = ::log4rs::init_config(config)?;
     Ok(())
 }
-
