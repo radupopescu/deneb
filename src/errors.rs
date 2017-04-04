62dfe18e083cbd9222aac4bfff610da119432df8
Radu Popescu <mail@radupopescu.net>
2017-04-04 08:10:06 +0200

    Extending custom error types

--- src/errors.rs
+++ src/errors.rs
@@ -1,9 +1,12 @@
 use log;
 use log4rs;
+use nix;
+use time::OutOfRangeError;
 
 use std::io;
+use std::time;
+
 use merkle;
-use nix;
 
 error_chain! {
     types {}
@@ -17,6 +20,8 @@ error_chain! {
         LogError(log::SetLoggerError) #[doc="log error"];
         Log4rsConfigError(log4rs::config::Errors) #[doc="log4rs config error"];
         NixError(nix::Error) #[doc="nix error"];
+        DurationOutOfRangeError(OutOfRangeError) #[doc="duration out-of-range error"];
+        SystemTimeError(time::SystemTimeError) #[doc="system time conversion error"];
     }
 
     errors {
