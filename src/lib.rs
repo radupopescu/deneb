482005c78c6baa2f152a17ee2436c289c91a9d59
Radu Popescu <mail@radupopescu.net>
2017-04-08 21:37:38 +0200

    Add fuse crate to dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,17 @@ extern crate clap;
 extern crate chrono;
 #[macro_use]
 extern crate error_chain;
+extern crate fuse;
 #[allow(unused_imports)]
 #[macro_use]
 extern crate log;
 extern crate log4rs;
 extern crate merkle;
 extern crate nix;
-extern crate notify;
 extern crate time;
 
 pub mod catalog;
 pub mod errors;
-pub mod fs;
 pub mod hash;
 pub mod logging;
 pub mod params;
-pub mod watch;
