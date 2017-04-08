b13825d2091c0d84dfdffe020fb53ed04ef50444
Radu Popescu <mail@radupopescu.net>
2017-04-09 00:32:29 +0200

    Disable JSON encoding for logging

--- src/logging.rs
+++ src/logging.rs
@@ -1,13 +1,11 @@
 use log::LogLevelFilter;
 use log4rs::append::console::ConsoleAppender;
-use log4rs::encode::json::JsonEncoder;
 use log4rs::config::{Appender, Config, Root};
 
 use errors::*;
 
 pub fn init() -> Result<()> {
-    let json_encoder = Box::new(JsonEncoder::new());
-    let stdout = ConsoleAppender::builder().encoder(json_encoder).build();
+    let stdout = ConsoleAppender::builder().build();
 
     let config = Config::builder().appender(Appender::builder().build("stdout", Box::new(stdout)))
         .build(Root::builder()
