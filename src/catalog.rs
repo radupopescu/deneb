2e661f5fca610123affa7f5a059ce774e30c453d
Radu Popescu <mail@radupopescu.net>
2017-04-09 14:26:14 +0200

    Parameterize Catalog struct on the hash type

--- src/catalog.rs
+++ src/catalog.rs
@@ -1,5 +1,6 @@
 use std::fmt;
 use std::fs::{DirEntry, FileType, Permissions, read_dir};
+use std::hash::Hash;
 use std::path::{Path, PathBuf};
 use std::time::{UNIX_EPOCH, SystemTime};
 
@@ -10,9 +11,7 @@ use nix::sys::stat::stat;
 
 use errors::*;
 
-use hash::Hash;
-
-struct Item {
+struct Item<H> {
     name: PathBuf,
     uid: u32,
     gid: u32,
@@ -21,7 +20,7 @@ struct Item {
     creation_time: Option<SystemTime>,
     access_time: Option<SystemTime>,
     modification_time: Option<SystemTime>,
-    content_hash: Hash,
+    content_hash: H,
 }
 
 fn system_to_date_time(st: &Option<SystemTime>) -> Result<DateTime<UTC>> {
@@ -35,7 +34,7 @@ fn system_to_date_time(st: &Option<SystemTime>) -> Result<DateTime<UTC>> {
     }
 }
 
-impl fmt::Display for Item {
+impl<H> fmt::Display for Item<H> where H: fmt::Debug {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match (system_to_date_time(&self.creation_time),
                system_to_date_time(&self.access_time),
@@ -70,16 +69,16 @@ impl fmt::Display for Item {
     }
 }
 
-pub struct Catalog {
-    items: Vec<Item>,
+pub struct Catalog<H> {
+    items: Vec<Item<H>>,
 }
 
-impl Catalog {
-    pub fn new() -> Catalog {
+impl<H> Catalog<H> where H: Hash + Default {
+    pub fn new() -> Catalog<H> {
         Catalog { items: Vec::new() }
     }
 
-    pub fn from_dir(dir: &Path) -> Result<Catalog> {
+    pub fn from_dir(dir: &Path) -> Result<Catalog<H>> {
         let mut catalog = Catalog::new();
         let _ = visit_dirs(dir, &mut |e| catalog.add_item(e))?;
         Ok(catalog)
@@ -97,7 +96,7 @@ impl Catalog {
             creation_time: metadata.created().ok(),
             access_time: metadata.accessed().ok(),
             modification_time: metadata.modified().ok(),
-            content_hash: Hash::default(),
+            content_hash: H::default(),
         });
         Ok(())
     }
