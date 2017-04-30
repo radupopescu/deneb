3514f62456fec428771fd91fa2809419307ccab5
Radu Popescu <mail@radupopescu.net>
2017-04-30 17:03:42 +0200

    rustfmt

--- src/fs.rs
+++ src/fs.rs
@@ -1,4 +1,5 @@
-use fuse::{Filesystem, Request, ReplyAttr, ReplyDirectory, ReplyEmpty, ReplyEntry, ReplyOpen};
+use fuse::{Filesystem, Request, ReplyAttr, ReplyData, ReplyDirectory, ReplyEmpty, ReplyEntry,
+           ReplyOpen, ReplyStatfs};
 use nix::libc::EINVAL;
 use time::Timespec;
 
@@ -99,7 +100,12 @@ impl<S> Filesystem for Fs<S> {
         }
     }
 
-    fn readdir(&mut self, _req: &Request, ino: u64, fh: u64, offset: u64, mut reply: ReplyDirectory) {
+    fn readdir(&mut self,
+               _req: &Request,
+               ino: u64,
+               fh: u64,
+               offset: u64,
+               mut reply: ReplyDirectory) {
         debug!("readdir - ino: {}, fh: {}, offset: {}", ino, fh, offset);
         let mut index = offset as usize;
         match self.open_dirs.get(&fh) {
@@ -107,7 +113,7 @@ impl<S> Filesystem for Fs<S> {
                 while index < entries.len() {
                     let (ref name, idx) = entries[index];
                     if let Some(inode) = self.catalog.get_inode(&idx) {
-                        if ! reply.add(idx, index as u64 + 1, inode.attributes.kind, name) {
+                        if !reply.add(idx, index as u64 + 1, inode.attributes.kind, name) {
                             index = index + 1;
                         } else {
                             break;
@@ -121,38 +127,162 @@ impl<S> Filesystem for Fs<S> {
             }
         }
     }
+
+    fn open(&mut self, _req: &Request, _ino: u64, _flags: u32, reply: ReplyOpen) {}
+
+    fn readlink(&mut self, _req: &Request, _ino: u64, reply: ReplyData) {}
+
+    fn read(&mut self,
+            _req: &Request,
+            _ino: u64,
+            _fh: u64,
+            _offset: u64,
+            _size: u32,
+            reply: ReplyData) {
+    }
+
+    fn release(&mut self,
+               _req: &Request,
+               _ino: u64,
+               _fh: u64,
+               _flags: u32,
+               _lock_owner: u64,
+               _flush: bool,
+               reply: ReplyEmpty) {
+    }
+
+    fn statfs(&mut self, _req: &Request, _ino: u64, reply: ReplyStatfs) {}
+
+    fn access(&mut self, _req: &Request, _ino: u64, _mask: u32, reply: ReplyEmpty) {}
+
+    /*
+    fn getxattr(&mut self,
+                _req: &Request,
+                _ino: u64,
+                _name: &OsStr,
+                _size: u32,
+                reply: ReplyXattr) {
+    }
+    fn listxattr(&mut self, _req: &Request, _ino: u64, _size: u32, reply: ReplyXattr) {}
+    fn getlk(&mut self,
+             _req: &Request,
+             _ino: u64,
+             _fh: u64,
+             _lock_owner: u64,
+             _start: u64,
+             _end: u64,
+             _typ: u32,
+             _pid: u32,
+             reply: ReplyLock) {
+    }
+
+    // Callbacks for write functionality
+    fn forget(&mut self, _req: &Request, _ino: u64, _nlookup: u64) {}
+    fn setattr(&mut self,
+               _req: &Request,
+               _ino: u64,
+               _mode: Option<u32>,
+               _uid: Option<u32>,
+               _gid: Option<u32>,
+               _size: Option<u64>,
+               _atime: Option<Timespec>,
+               _mtime: Option<Timespec>,
+               _fh: Option<u64>,
+               _crtime: Option<Timespec>,
+               _chgtime: Option<Timespec>,
+               _bkuptime: Option<Timespec>,
+               _flags: Option<u32>,
+               reply: ReplyAttr) {
+    }
+    fn mknod(&mut self,
+             _req: &Request,
+             _parent: u64,
+             _name: &OsStr,
+             _mode: u32,
+             _rdev: u32,
+             reply: ReplyEntry) {
+    }
+    fn mkdir(&mut self,
+             _req: &Request,
+             _parent: u64,
+             _name: &OsStr,
+             _mode: u32,
+             reply: ReplyEntry) {
+    }
+    fn unlink(&mut self, _req: &Request, _parent: u64, _name: &OsStr, reply: ReplyEmpty) {}
+    fn rmdir(&mut self, _req: &Request, _parent: u64, _name: &OsStr, reply: ReplyEmpty) {}
+    fn symlink(&mut self,
+               _req: &Request,
+               _parent: u64,
+               _name: &OsStr,
+               _link: &Path,
+               reply: ReplyEntry) {
+    }
+    fn rename(&mut self,
+              _req: &Request,
+              _parent: u64,
+              _name: &OsStr,
+              _newparent: u64,
+              _newname: &OsStr,
+              reply: ReplyEmpty) {
+    }
+    fn link(&mut self,
+            _req: &Request,
+            _ino: u64,
+            _newparent: u64,
+            _newname: &OsStr,
+            reply: ReplyEntry) {
+    }
+    fn write(&mut self,
+             _req: &Request,
+             _ino: u64,
+             _fh: u64,
+             _offset: u64,
+             _data: &[u8],
+             _flags: u32,
+             reply: ReplyWrite) {
+    }
+    fn flush(&mut self, _req: &Request, _ino: u64, _fh: u64, _lock_owner: u64, reply: ReplyEmpty) {}
+    fn fsync(&mut self, _req: &Request, _ino: u64, _fh: u64, _datasync: bool, reply: ReplyEmpty) {}
+    fn fsyncdir(&mut self,
+                _req: &Request,
+                _ino: u64,
+                _fh: u64,
+                _datasync: bool,
+                reply: ReplyEmpty) {
+    }
+    fn setxattr(&mut self,
+                _req: &Request,
+                _ino: u64,
+                _name: &OsStr,
+                _value: &[u8],
+                _flags: u32,
+                _position: u32,
+                reply: ReplyEmpty) {
+    }
+    fn removexattr(&mut self, _req: &Request, _ino: u64, _name: &OsStr, reply: ReplyEmpty) {}
+    fn create(&mut self,
+              _req: &Request,
+              _parent: u64,
+              _name: &OsStr,
+              _mode: u32,
+              _flags: u32,
+              reply: ReplyCreate) {
+    }
+    fn setlk(&mut self,
+             _req: &Request,
+             _ino: u64,
+             _fh: u64,
+             _lock_owner: u64,
+             _start: u64,
+             _end: u64,
+             _typ: u32,
+             _pid: u32,
+             _sleep: bool,
+             reply: ReplyEmpty) {
+    }
+
+    // Other callbacks
+    fn bmap(&mut self, _req: &Request, _ino: u64, _blocksize: u32, _idx: u64, reply: ReplyBmap) {}
+     */
 }
-
-/*
-fn open(&mut self, _req: &Request, _ino: u64, _flags: u32, reply: ReplyOpen) {}
-fn readlink(&mut self, _req: &Request, _ino: u64, reply: ReplyData) { ... }
-fn read(&mut self, _req: &Request, _ino: u64, _fh: u64, _offset: u64, _size: u32, reply: ReplyData) {}
-fn release(&mut self, _req: &Request, _ino: u64, _fh: u64, _flags: u32, _lock_owner: u64, _flush: bool, reply: ReplyEmpty) {}
-fn statfs(&mut self, _req: &Request, _ino: u64, reply: ReplyStatfs) {}
-fn getxattr(&mut self, _req: &Request, _ino: u64, _name: &OsStr, _size: u32, reply: ReplyXattr) {}
-fn listxattr(&mut self, _req: &Request, _ino: u64, _size: u32, reply: ReplyXattr) {}
-fn access(&mut self, _req: &Request, _ino: u64, _mask: u32, reply: ReplyEmpty) {}
-fn getlk(&mut self, _req: &Request, _ino: u64, _fh: u64, _lock_owner: u64, _start: u64, _end: u64, _typ: u32, _pid: u32, reply: ReplyLock) {}
-
-// Callbacks for write functionality
-fn forget(&mut self, _req: &Request, _ino: u64, _nlookup: u64) { ... }
-fn setattr(&mut self, _req: &Request, _ino: u64, _mode: Option<u32>, _uid: Option<u32>, _gid: Option<u32>, _size: Option<u64>, _atime: Option<Timespec>, _mtime: Option<Timespec>, _fh: Option<u64>, _crtime: Option<Timespec>, _chgtime: Option<Timespec>, _bkuptime: Option<Timespec>, _flags: Option<u32>, reply: ReplyAttr) {}
-fn mknod(&mut self, _req: &Request, _parent: u64, _name: &OsStr, _mode: u32, _rdev: u32, reply: ReplyEntry) {}
-fn mkdir(&mut self, _req: &Request, _parent: u64, _name: &OsStr, _mode: u32, reply: ReplyEntry) {}
-fn unlink(&mut self, _req: &Request, _parent: u64, _name: &OsStr, reply: ReplyEmpty) {}
-fn rmdir(&mut self, _req: &Request, _parent: u64, _name: &OsStr, reply: ReplyEmpty) {}
-fn symlink(&mut self, _req: &Request, _parent: u64, _name: &OsStr, _link: &Path, reply: ReplyEntry) {}
-fn rename(&mut self, _req: &Request, _parent: u64, _name: &OsStr, _newparent: u64, _newname: &OsStr, reply: ReplyEmpty) {}
-fn link(&mut self, _req: &Request, _ino: u64, _newparent: u64, _newname: &OsStr, reply: ReplyEntry) {}
-fn write(&mut self, _req: &Request, _ino: u64, _fh: u64, _offset: u64, _data: &[u8], _flags: u32, reply: ReplyWrite) {}
-fn flush(&mut self, _req: &Request, _ino: u64, _fh: u64, _lock_owner: u64, reply: ReplyEmpty) {}
-fn fsync(&mut self, _req: &Request, _ino: u64, _fh: u64, _datasync: bool, reply: ReplyEmpty) {}
-fn fsyncdir(&mut self, _req: &Request, _ino: u64, _fh: u64, _datasync: bool, reply: ReplyEmpty) {}
-fn setxattr(&mut self, _req: &Request, _ino: u64, _name: &OsStr, _value: &[u8], _flags: u32, _position: u32, reply: ReplyEmpty) {}
-fn removexattr(&mut self, _req: &Request, _ino: u64, _name: &OsStr, reply: ReplyEmpty) {}
-fn create(&mut self, _req: &Request, _parent: u64, _name: &OsStr, _mode: u32, _flags: u32, reply: ReplyCreate) {}
-fn setlk(&mut self, _req: &Request, _ino: u64, _fh: u64, _lock_owner: u64, _start: u64, _end: u64, _typ: u32, _pid: u32, _sleep: bool, reply: ReplyEmpty) {}
-
-// Other callbacks
-fn bmap(&mut self, _req: &Request, _ino: u64, _blocksize: u32, _idx: u64, reply: ReplyBmap) {}
-*/
