f842e0ffa97d1ff3fa9fc147fc8f23e54987147e
Radu Popescu <mail@radupopescu.net>
2017-05-15 08:16:08 +0200

    Add a handler for SIGINT such that the FUSE mount is unmounted on exit

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -2,3 +2,4 @@
 
 pub mod errors;
 pub mod logging;
+pub mod util;
---
+++ src/common/util.rs
@@ -0,0 +1,29 @@
+use nix::sys::signal::{SigmaskHow, Signal, SigSet, pthread_sigmask};
+
+use std::sync::mpsc::Sender;
+use std::thread::{JoinHandle, spawn};
+
+use common::errors::*;
+
+pub fn block_signals() -> Result<()> {
+    let mut sigs = SigSet::empty();
+    sigs.add(Signal::SIGINT);
+    pthread_sigmask(SigmaskHow::SIG_BLOCK, Some(&sigs), None)?;
+    Ok(())
+}
+
+pub fn set_sigint_handler(tx: Sender<()>) -> Result<JoinHandle<()>> {
+    Ok(spawn(move || {
+        let mut sigs = SigSet::empty();
+        sigs.add(Signal::SIGINT);
+        if let Ok(sig) = sigs.wait() {
+            match sig {
+                Signal::SIGINT => {
+                    debug!("Ctrl-C received. Exiting.");
+                    let _ = tx.send(());
+                }
+                _ => {}
+            }
+        }
+    }))
+}
