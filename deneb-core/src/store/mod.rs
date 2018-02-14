db93a5b2430a66a8bc7915bdc6e39dcc79d01d02
Radu Popescu <mail@radupopescu.net>
2018-02-14 13:20:50 +0100

    Add some documentation to `Store` trait

--- deneb-core/src/store/mod.rs
+++ deneb-core/src/store/mod.rs
@@ -12,19 +12,35 @@ pub use self::mem::{MemStore, MemStoreBuilder};
 mod disk;
 pub use self::disk::{DiskStore, DiskStoreBuilder};
 
+/// Builder types for `Store` objects
 pub trait StoreBuilder {
     type Store: self::Store;
 
+    /// Construct the new store at the specified directory
+    ///
+    /// It is assumed that the newly constructed store will keep any
+    /// objects (chunks) already present at the specified directory
     fn at_dir<P: AsRef<Path>>(&self, dir: P, chunk_size: usize) -> DenebResult<Self::Store>;
 }
 
+/// Types which can perform IO into repository storage
+///
 pub trait Store {
+    /// Returns the chunk size used by the store
     fn chunk_size(&self) -> usize;
 
+    /// Returns a buffer with the contents of the requested chunk
+    ///
+    /// The method returns the "unpacked" chunks wrapped in an `Arc`,
+    /// allowing implementations to cache the results.
     fn get_chunk(&self, digest: &Digest) -> DenebResult<Arc<Vec<u8>>>;
 
+    /// Write a single chunk into the repository
+    ///
     fn put_chunk(&mut self, digest: &Digest, contents: Vec<u8>) -> DenebResult<()>;
 
+    /// Write a file into the repository without chunking
+    ///
     fn put_file<R: Read>(&mut self, mut data: R) -> DenebResult<ChunkDescriptor> {
         let mut buf = vec![];
         let n = data.read_to_end(&mut buf)?;
@@ -34,6 +50,8 @@ pub trait Store {
         Ok(descriptor)
     }
 
+    /// Write a file into the repository with chunking
+    ///
     fn put_file_chunked<R: Read>(&mut self, data: R) -> DenebResult<Vec<ChunkDescriptor>> {
         let mut descriptors = vec![];
         let mut buf = vec![0 as u8; self.chunk_size()];
